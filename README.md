2a4191cdfdb9881c321ab1114c840a14b1287dfe
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-20 08:46:00 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+//id
+//name
+//img_url
+//inhabitants
+//notable_residents
--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 
-document.querySelector('button').addEventListener('click', getFetch)
+// document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch() {
+window.onload = function getFetch() {
     const choice = (document.querySelector('input').value).toLowerCase()
-    const url = 'https://pokeapi.co/api/v2/pokemon/' + choice
+    const url = 'https://finalspaceapi.com/api/v0/location/'
 
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            console.log(data)
-            document.querySelector('img').src = data.sprites.front_shiny
+            console.log(data[0])
+            // document.querySelector('#planetId').src = data[0].id
+            document.querySelector('h2').innerText = data[0].name
+            document.querySelector('img').src = data[0].img_url
+            document.querySelector('li').innerText = data[0].inhabitants[0]
+            document.querySelector('h4').innerText = data[0].notable_residents
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
 }
+
+
+
+
