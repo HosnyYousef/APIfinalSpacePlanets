c99c7ad383d30e4017951c5afe82a443540676f7
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-24 13:52:48 -0800

    joined them with a new line \n seperated, inhabitants

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ window.onload = function getFetch() {
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            console.log(data[0].inhabitants.join("\n"))
+            console.log(data[0])
             // document.querySelector('#planetId').src = data[0].id
             document.querySelector('h2').innerText = data[0].name
             document.querySelector('img').src = data[0].img_url
-            document.querySelector('h3').innerText = data[0].inhabitants.join("\n")
+            document.querySelector('h5').innerText = data[0].inhabitants.join("\n")
+            document.querySelector('h4').innerText = data[0].notable_residents
+            .join("\n")
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -20,5 +22,21 @@ window.onload = function getFetch() {
 }
 
 
+
+window.onload = function getFetch() {
+    const choice = (document.querySelector('input').value).toLowerCase()
+    const url = 'https://finalspaceapi.com/api/v0/character/'
+
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data[0])
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
+
+// https://finalspaceapi.com/api/v0/character/
 
 
