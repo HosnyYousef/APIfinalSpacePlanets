470a58b9952fcd6fd2685f2430ac58cfdfba2cf6
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-24 12:51:24 -0800

    joined them with a new line \n seperated, inhabitants

--- js/main.js
+++ js/main.js
@@ -5,25 +5,14 @@ window.onload = function getFetch() {
     const choice = (document.querySelector('input').value).toLowerCase()
     const url = 'https://finalspaceapi.com/api/v0/location/'
 
-    let displayList = document.querySelector('li').innerText;
-
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            console.log(data[0].inhabitants[0])
+            console.log(data[0].inhabitants.join("\n"))
             // document.querySelector('#planetId').src = data[0].id
             document.querySelector('h2').innerText = data[0].name
             document.querySelector('img').src = data[0].img_url
-            document.querySelector('li').innerText = data[0].inhabitants;
-            let displayList = []
-            data[0].inhabitants.forEach((item) => {
-                displayList.push(item.inhabitants);
-                let li = document.createElement("li");
-                li.innerText = item;
-                list.appendChild(li);
-            })
-            console.log(displayList)
-            document.querySelector('h4').innerText = data[0].notable_residents
+            document.querySelector('h3').innerText = data[0].inhabitants.join("\n")
         })
         .catch(err => {
             console.log(`error ${err}`)
