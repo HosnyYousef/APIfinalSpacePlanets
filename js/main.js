21fc21510e37e5ce7b0af8c1baac3fdd3939780e
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-24 19:58:49 -0800

    random refresh

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 
-// document.querySelector('button').addEventListener('click', getFetch)
+const planets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const random = (Math.floor(Math.random() * planets.length))
+let planetList = (planets[random]);
+console.log(planetList)
 
 window.onload = function getFetch() {
     const choice = (document.querySelector('input').value).toLowerCase()
     const url = 'https://finalspaceapi.com'
 
-    fetch(`${url}/api/v0/location/`)
+    fetch(`${url}/api/v0/location/`+planetList)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            const notableResId = data.character;
-            console.log(notableResId, '\n')
-            console.log(data[0])
-            document.querySelector('#planetId').innerText = data[0].id
-            document.querySelector('h2').innerText = data[0].name
-            document.querySelector('img').src = data[0].img_url
-            document.querySelector('h5').innerText = data[0].inhabitants.join("\n")
+            // const notableResId = data.character;
+            // console.log(notableResId, '\n')
+            console.log(data)
+            document.querySelector('#planetId').innerText = data.id
+            document.querySelector('h2').innerText = data.name
+            document.querySelector('img').src = data.img_url
+            document.querySelector('h5').innerText = data.inhabitants.join("\n")
         //     return fetch(`${url}/character/${notableResId}`); // make a 2nd request and return a promise
         // })
         // .then(response => response.json())
