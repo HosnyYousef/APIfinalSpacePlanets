d218158ba1094347632b18228d75bbb4332ec43a
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-24 18:03:11 -0800

    trying to work notiable characters

--- js/main.js
+++ js/main.js
@@ -3,18 +3,24 @@
 
 window.onload = function getFetch() {
     const choice = (document.querySelector('input').value).toLowerCase()
-    const url = 'https://finalspaceapi.com/api/v0/location/'
+    const url = 'https://finalspaceapi.com'
 
-    fetch(url)
+    fetch(`${url}/api/v0/location/`)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
+            const notableResId = data.character;
+            console.log(notableResId, '\n')
             console.log(data[0])
-            // document.querySelector('#planetId').src = data[0].id
+            document.querySelector('#planetId').innerText = data[0].id
             document.querySelector('h2').innerText = data[0].name
             document.querySelector('img').src = data[0].img_url
             document.querySelector('h5').innerText = data[0].inhabitants.join("\n")
-            document.querySelector('h4').innerText = data[0].notable_residents
-            .join("\n")
+        //     return fetch(`${url}/character/${notableResId}`); // make a 2nd request and return a promise
+        // })
+        // .then(response => response.json())
+        // .then(data => {
+        //     document.querySelector('h4').innerText = data[0].notable_residents
+        //     .join("\n")
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -30,12 +36,16 @@ window.onload = function getFetch() {
 //     fetch(url)
 //         .then(res => res.json()) // parse response as JSON
 //         .then(data => {
-//             console.log(data[0])
+//             console.log(data)
+//             document.querySelector('h4').innerText = data[0].notable_residents
+//             .join("\n")
 //         })
 //         .catch(err => {
 //             console.log(`error ${err}`)
 //         });
 // }
+
+//
 
 // https://finalspaceapi.com/api/v0/character/
 
